615a818800626d6b3978237647347a21537d9db0
aavendan <aavenda@gmail.com>
2023-07-24 19:17:24 -0500

    done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { MainComponent } from './pages/main/main.component';
+import { ReportComponent } from './pages/report/report.component';
+import { FormComponent } from './pages/form/form.component';
+import { AboutComponent } from './pages/about/about.component';
+
+const routes: Routes = [
+  { path: "main", component: MainComponent },
+  { path: "report", component: ReportComponent },
+  { path: "form", component: FormComponent },
+  { path: "about", component: AboutComponent },
+  { path: "**", redirectTo: "main" }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,35 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainComponent } from './pages/main/main.component';
+import { ReportComponent } from './pages/report/report.component';
+import { FormComponent } from './pages/form/form.component';
+import { MenuComponent } from './pages/menu/menu.component';
+import { AboutComponent } from './pages/about/about.component';
+
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    ReportComponent,
+    FormComponent,
+    MenuComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
